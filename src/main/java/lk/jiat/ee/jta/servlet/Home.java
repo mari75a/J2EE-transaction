9e40beae76fbb7a70960cd961b2c282d003a1933
mari75a <sangeethlakshan0@gmail.com>
2025-06-01 10:35:06 +0530

    add persistence

--- src/main/java/lk/jiat/ee/jta/servlet/Home.java
+++ src/main/java/lk/jiat/ee/jta/servlet/Home.java
@@ -1,5 +1,9 @@
 package lk.jiat.ee.jta.servlet;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,12 +11,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/home")
 public class Home extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+      EntityManagerFactory factory= Persistence.createEntityManagerFactory("JTAPU");
+      EntityManager em= factory.createEntityManager();
+
+      Query query=em.createNativeQuery("select * from user");
+      List<Object[]> list=query.getResultList();
+      list.forEach( li->{
+            System.out.println(li[0] + " "+ li[1] +" "+ li[2]);
+        });
+        System.out.println(list);
     }
 }
